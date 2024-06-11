5bd2490a3c2edc216d9b22ecc933ddab50aa19e9
mohamedessamelnaggar <mohamedessamelnaggar50@gmail.com>
2024-06-11 16:26:28 +0300

    TicTac

---
+++ src/components/Player.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+    const [isEditing , setIsEditing] = useState(false);
+
+    function HandleEditClick(){
+        setIsEditing(!isEditing);
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+
+    if (isEditing) {
+        playerName = <input type="text" required value={name} />
+    }
+
+
+    return (
+        <li>
+            <span className="player">
+                {playerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={HandleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+        </li>
+
+    )
+
+}
